ff2f3582921962f6793461cea2409b4e45300fe6
MacFan4000 <paulfkeffer@gmail.com>
2020-09-23 19:04:41 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt') as requirements_file:
+    requirements = [req for req in requirements_file.readlines()]
+
+
+setup(
+    name='MirahezeBot_jsonparser',
+    version='1.0.0',
+    description='jsonparser utility for MirahezeBots',
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',  # This is important!
+    author='MirahezeBot Contributors',
+    author_email='bots@miraheze.org',
+    url='https://github.com/MirahezeBots/jsonparser',
+    include_package_data=True,
+    install_requires=requirements,
+    license='Eiffel Forum License, version 2',
+)
