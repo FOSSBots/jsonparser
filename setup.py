1609e2e1c7256dab213498a8802f880b7c3f7741
MacFan4000 <paulfkeffer@gmail.com>
2020-09-23 20:21:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('requirements.txt') as requirements_file:
-    requirements = [req for req in requirements_file.readlines()]
-
 
 setup(
     name='jsonparser',
@@ -19,6 +16,5 @@ setup(
     author_email='bots@miraheze.org',
     url='https://github.com/MirahezeBots/jsonparser',
     include_package_data=True,
-    install_requires=requirements,
     license='Eiffel Forum License, version 2',
 )
