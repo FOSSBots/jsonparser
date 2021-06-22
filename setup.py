8200c6ee9d17beb50a2167f4cf00e5ad4d0dde43
RhinosF1 <rhinosf1@gmail.com>
2021-06-22 16:00:15 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as requirements_file:
 
 setup(
     name='MirahezeBots_jsonparser',
-    version='1.0.3',
+    version='1.0.4',
     description='jsonparser utility for MirahezeBots',
     long_description=readme,
     long_description_content_type='text/markdown',  # This is important!
